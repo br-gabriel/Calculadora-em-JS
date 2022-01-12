e5fe431b5ea8390dc69dd60c6982ed7ea7158a1f
br-gabriel <contato.gabrielfeitosa@gmail.com>
2022-01-11 19:14:25 -0500

    atualizacao no css

---
+++ to-do list/main.js
@@ -0,0 +1,11 @@
+//Ao clicar em adicionar ou pressionar enter, copia o textContent
+//para o novo input checkbox e cria o elemento, apagando o conteudo 
+//do input de tarefa
+
+//Caso o textContent esteja vazio não cria uma nova tarefa
+
+
+//Ao clicar em concluido o texto fica riscado
+
+//Opção de remover a tarefa com um botão "X"
+//Botão para remover todas as tarefas de uma vez
